fd32c7abe92430a499f0322b29745bdbf4f2435d
samgh <sghaneka@yahoo.com>
2025-09-23 16:48:36 -0400

    attempt 2

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -3,7 +3,7 @@ const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
 
-// hi1
+// hi2
 
 const devConfig = {
   mode: "development",
