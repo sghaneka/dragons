57df311e396e04c2f32461ed4f25ba96b20ef05a
samgh <sghaneka@yahoo.com>
2025-09-23 14:08:01 -0400

    w

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -3,6 +3,8 @@ const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
 
+// hi
+
 const devConfig = {
   mode: "development",
   devServer: {
