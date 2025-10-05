91c93449cdd79dc233307a25810477e08dc7f350
samgh <sghaneka@yahoo.com>
2025-10-05 12:30:31 -0400

    added auth

---
+++ packages/auth/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Switch, Route, Router } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "au",
+});
+
+const App = ({ history, onSignIn }) => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auth/signin">
+              <Signin onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <Signup onSignIn={onSignIn} />
+            </Route>
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
+
+export default App;
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { lazy, Suspense, useState } from "react";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
-import { BrowserRouter } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 import Header from "./components/Header";
+import Progress from "./components/Progress";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <div>
-          <Header />
-          <MarketingApp />
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setIsSignedIn(false)}
+          />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </BrowserRouter>
     </StylesProvider>
